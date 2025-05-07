78f3544a7662b537b03aac020fe4f153a18f8543
Mike Potanin <potaninpm@gmail.com>
2025-05-07 20:36:49 +0300

    refactor: some strings additions

--- app/src/main/java/com/team/niceeljur/navigation/components/MoreBottomSheet.kt
+++ app/src/main/java/com/team/niceeljur/navigation/components/MoreBottomSheet.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
